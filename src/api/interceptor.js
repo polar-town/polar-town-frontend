a94c7f5c0a796981868ab6c053aef62f9918698f
choyeun98 <choyeun98@naver.com>
2022-02-08 16:38:23 +0900

    feature: add axios interceptor

---
+++ src/api/interceptor.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { selectUserToken } from "../features/user/userSlice";
+import useRefreshToken from "../utils/getAccessToken";
+
+const currentUserAccessToken = useSelector(selectUserToken);
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    if (!config.headers["Authorization"]) {
+      config.headers["Authorization"] = `Barearer ${currentUserAccessToken}`;
+    }
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (err) => {
+    const originalRequest = err.config;
+    if (err.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const newAccessToken = await useRefreshToken();
+      originalRequest.headers["Authorization"] = `Barearer ${newAccessToken}`;
+
+      return instance(originalRequest);
+    }
+
+    return Promise.reject(err);
+  },
+);
+
+export default instance;
