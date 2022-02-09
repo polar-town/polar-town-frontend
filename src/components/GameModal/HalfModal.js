ea1216f05f5788be4547481beaa331d545b3f3b1
choiree <riririgood@naver.com>
2022-02-09 11:14:12 +0900

    refactor: change prototypes module

--- src/components/GameModal/HalfModal.js
+++ src/components/GameModal/HalfModal.js
@@ -1,6 +1,6 @@
-import proptypes from "proptypes";
 import React, { useState } from "react";
 import styled from "styled-components";
+import proptypes from "prop-types";
 
 const StyledHalfModal = styled.div`
   margin: 0 auto;
