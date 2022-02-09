9b1c0410d329815598a85bc485074ac6ba459080
geeonie <nomad.jiwhan@gmail.com>
2022-02-09 16:54:36 +0900

    refactor: set constants variables for notification contents handling

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -6,6 +6,17 @@ import { selectNotificationType } from "../../features/modal/modalSlice";
 import GameModalButton from "../GameModal/GameModalButton";
 import { nanoid } from "nanoid";
 
+import {
+  NOTIFICATION_TYPE_CONFIRM_PURCHASE,
+  NOTIFICATION_TYPE_FRIEND_REQUEST,
+  NOTIFICATION_MESSAGE_CONFIRM_PURCHASE,
+  NOTIFICATION_MESSAGE_FRIEND_REQUEST,
+  NOTIFICATION_MESSAGE_PRESENT,
+  NOTIFICATION_OPTION_CONFIRM_PURCHASE,
+  NOTIFICATION_OPTION_FRIEND_REQUEST,
+  NOTIFICATION_OPTION_PRESENT,
+} from "../../constants/notification";
+
 const NotificationContainer = styled.div`
   height: 300px;
   overflow: actions;
@@ -28,21 +39,20 @@ const ButtonContainer = styled.div`
 `;
 
 function Notification() {
-  // const notificationType = useSelector(selectNotificationType);
-  const notificationType = "friendRequest";
+  const notificationType = useSelector(selectNotificationType);
   const [buttonContent, setButtonContent] = useState([]);
   const [notificationMessage, setNotificationMessage] = useState("");
 
   useEffect(() => {
-    if (notificationType === "confirmPurchase") {
-      setButtonContent(["네", "아니요"]);
-      setNotificationMessage("아이템을 구매하시겠습니까?");
-    } else if (notificationType === "friendRequest") {
-      setButtonContent(["수락", "거절"]);
-      setNotificationMessage("친구 신청이 왔습니다. 수락하시겠습니까?");
+    if (notificationType === NOTIFICATION_TYPE_CONFIRM_PURCHASE) {
+      setButtonContent(NOTIFICATION_OPTION_CONFIRM_PURCHASE);
+      setNotificationMessage(NOTIFICATION_MESSAGE_CONFIRM_PURCHASE);
+    } else if (notificationType === NOTIFICATION_TYPE_FRIEND_REQUEST) {
+      setButtonContent(NOTIFICATION_OPTION_FRIEND_REQUEST);
+      setNotificationMessage(NOTIFICATION_MESSAGE_FRIEND_REQUEST);
     } else {
-      setButtonContent(["확인"]);
-      setNotificationMessage("선물이 도착했습니다. 확인하시겠습니까?");
+      setButtonContent(NOTIFICATION_OPTION_PRESENT);
+      setNotificationMessage(NOTIFICATION_MESSAGE_PRESENT);
     }
   }, [notificationType]);
 
---
+++ src/constants/notification.js
@@ -0,0 +1,12 @@
+export const NOTIFICATION_TYPE_CONFIRM_PURCHASE = "confirmPurchase";
+export const NOTIFICATION_TYPE_FRIEND_REQUEST = "friendRequest";
+export const NOTIFICATION_TYPE_PRESENT = "present";
+export const NOTIFICATION_MESSAGE_CONFIRM_PURCHASE =
+  "아이템을 구매하시겠습니까?";
+export const NOTIFICATION_MESSAGE_FRIEND_REQUEST =
+  "친구 신청이 왔습니다. 수락하시겠습니까?";
+export const NOTIFICATION_MESSAGE_PRESENT =
+  "선물이 도착했습니다. 확인하시겠습니까?";
+export const NOTIFICATION_OPTION_CONFIRM_PURCHASE = ["예", "아니요"];
+export const NOTIFICATION_OPTION_FRIEND_REQUEST = ["수락", "거절"];
+export const NOTIFICATION_OPTION_PRESENT = ["확인"];
