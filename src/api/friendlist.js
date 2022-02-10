66e4697000cc6d1ef6ac6fc0909d8ef59061af85
geeonie <nomad.jiwhan@gmail.com>
2022-02-11 05:22:46 +0900

    feature : remove pending friend when user response friend requests

--- src/api/friendlist.js
+++ src/api/friendlist.js
@@ -7,13 +7,14 @@ export async function getFriendList(userId) {
     );
 
     const friendList = response.data.result.map((res) => {
-      const { _id, name, photo, iceCount } = res.userId;
+      const { _id, name, photo, iceCount, email } = res.userId;
 
       return {
         id: _id,
         name,
         photo,
         iceCount,
+        email,
       };
     });
 
@@ -23,10 +24,61 @@ export async function getFriendList(userId) {
   }
 }
 
-export async function deleteFriend(userId) {
+export async function deleteFriend(userId, email) {
   try {
     await axios.delete(
       `${process.env.REACT_APP_BASE_URL}/users/${userId}/friends`,
+      { data: { email } },
+      { withCredentials: true },
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getPendingFriendList(userId) {
+  try {
+    const response = await axios.get(
+      `${process.env.REACT_APP_BASE_URL}/users/${userId}/friends/pending`,
+      { withCredentials: true },
+    );
+
+    const pendingFriendList = response.data.result.map((res) => {
+      const { _id, name, photo, iceCount, email } = res.userId;
+
+      return {
+        id: _id,
+        name,
+        photo,
+        iceCount,
+        email,
+      };
+    });
+
+    return pendingFriendList;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function addFriendList(userId, email) {
+  try {
+    await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/users/${userId}/friends`,
+      { email },
+      { withCredentials: true },
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deletePendingFriend(userId, email) {
+  try {
+    await axios.delete(
+      `${process.env.REACT_APP_BASE_URL}/users/${userId}/friends/pending`,
+      { data: { email } },
+      { withCredentials: true },
     );
   } catch (error) {
     console.error(error);
--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { closeFriendList } from "../../features/modal/modalSlice";
 import GameModal from "../GameModal/GameModal";
 import HalfModal from "../GameModal/HalfModal";
 import Friends from "./Friends";
@@ -8,21 +7,6 @@ import PendingFriends from "./PendingFriends";
 import proptypes from "prop-types";
 
 function FriendList({ toggleFriendList, visitFriend }) {
-  const dispatch = useDispatch();
-
-  const data = [
-    {
-      name: "조은별",
-      id: "62026c8514ae0f388ed66708",
-      url: "https://lh3.googleusercontent.com/a/AATXAJxxbHrLYUxCeas7MEWa79xgPZVVMHek_XFrhypx=s96-c",
-    },
-    {
-      name: "최리",
-      id: "62024632a669ed3ebbfbfe38",
-      url: "https://lh3.googleusercontent.com/a/AATXAJxgk2MYxYmGTfM8LRWnEFbooyhzGrR7rJBpm3ve=s96-c",
-    },
-  ];
-
   return (
     <GameModal
       onClose={() => {
